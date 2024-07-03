8f71b39e7b4ac36bb71f793a06f2cf973e11510a
nilton667 <nilton667@gmail.com>
2024-07-03 17:44:26 +0100

    0.0.2

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -20,6 +20,43 @@
     </div>
 
     <div class="onload">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">Lumberlift</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sobre Nós</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Serviços
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">ASNT NDT</a></li>
+                            <li><a class="dropdown-item" href="#">ENSAIO DE CARGA</a></li>
+                            <li><a class="dropdown-item" href="#">GESTÃO DE LOFT RIGG</a></li>
+                            <li><a class="dropdown-item" href="#">RETERMINAÇÃO DE SOQUETE</a></li>
+                            <li><a class="dropdown-item" href="#">EXAME VISUAL</a></li>
+                            <li><a class="dropdown-item" href="#">INSPEÇÃO DE CONTÊINER OFFSHORE</a></li>
+                            <li><a class="dropdown-item" href="#">SI 2306 EXAME</a></li>
+                            <li><a class="dropdown-item" href="#">SI 2307 EXAME</a></li>
+                            <li><a class="dropdown-item" href="#">CCUs MANUTENÇÃO COMPLETA</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Contacto</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
         @yield('content')
         <footer class="p-4">
             <div class="row">
