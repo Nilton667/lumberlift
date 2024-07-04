457bbc6be22dc630bd5ec0e8cfc717431fe79449
nilton667 <nilton667@gmail.com>
2024-07-04 09:03:11 +0100

    0.0.6

--- resources/views/contacto/index.blade.php
+++ resources/views/contacto/index.blade.php
@@ -3,3 +3,14 @@
 @section('title', 'Contacto')
 
 @section('content')
+<div class="row" style="max-width: 100%; padding: 0; margin: 0;">
+    <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2786.6823799826147!2d13.203359981121393!3d-8.975458394315812!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1a521fb9a8195e11%3A0xede53c68c3111603!2sAvenida%20Cmdt.%20Fidel%20Castro%2C%20Belas!5e0!3m2!1spt-PT!2sao!4v1720079870067!5m2!1spt-PT!2sao"
+    width="100%"
+    height="450"
+    style="border: 0; margin: 0; padding: 0;"
+    allowfullscreen=""
+    loading="lazy"
+    referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+@endsection
